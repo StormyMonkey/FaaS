5c01179f2d7b64c63a1ad5ae4637ca35ef2bc4c9
Guido Hungerbuehler <guh@open.ch>
2015-10-03 17:03:05 +0200

    Dashboard

--- client/template/views/activityDashboard.js
+++ client/template/views/activityDashboard.js
@@ -5,3 +5,87 @@ if (Meteor.isClient) {
         $("#engine-switch").bootstrapSwitch();
     };
 }
+
+Template.activityDashboard.helpers({
+        days: function() {
+            var days = new Array();
+            var day  = new Object();
+            day['id']   = 1;
+            day['date'] = '12.03.2015';
+            days.push(day);
+            day  = new Object();
+            day['id']   = 1;
+            day['date'] = '11.03.2015';
+            days.push(day);
+            console.log("days");
+            console.log(days);
+            return days;
+        },
+        session: function(elem) {
+            console.log("this");
+            console.log(this);
+            console.log("elem");
+            console.log(elem);
+            var sessions = new Array();
+            var session  = new Object();
+            session['id'] = 1;
+            session['activity'] = 'running';
+            session['activity_value'] = 30;
+            session['start']    = '16:04';
+            session['end']      = '17:06';
+            sessions.push(session);
+            session  = new Object();
+            session['id'] = 1;
+            session['activity'] = 'running';
+            session['activity_value'] = 20;
+            session['start']    = '18:04';
+            session['end']      = '19:06';
+            sessions.push(session);
+            console.log(sessions);
+            return sessions;
+        },
+        pie : function() {
+            console.log("PieChart");
+            // TODO get all sessions for this day
+            
+            console.log(this);
+            return {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false
+                },
+                width: "100",
+                height: "100",
+                title: {
+                    text: null
+                },
+                tooltip: {
+                    pointFormat: '<b>{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            },
+                            connectorColor: 'silver'
+                        }
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    //name: 'genre',
+                    data: [
+                        ['running'         ,   45.0],
+                        ['walk'            ,      26.0],
+                        ['no activity',          29.0]
+                    ]
+                }]
+            };
+        },
+});
