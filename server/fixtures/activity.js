1ef5260f25198d70d23223c6d345d95a6e0274d2
Daniel Aschwanden <nimdanitro@gmail.com>
2015-10-03 09:57:53 +0200

    prepare collections

---
+++ server/fixtures/activity.js
@@ -0,0 +1,25 @@
+if (Activity.find().count() === 0) {
+    console.log("Creating Sports fixtures");
+
+    Activity.insert({
+        text: "cycling",
+        image: "cycle.gif",
+    });
+
+    Activity.insert({
+        text: "running",
+        image: "run.gif",
+    });
+    Activity.insert({
+        text: "swimming",
+        image: "swim.gif",
+    });
+    Activity.insert({
+        text: "skating",
+        image: "skate.gif",
+    });
+    Activity.insert({
+        text: "hiking",
+        image: "walk.gif",
+    });
+}
