65c1b9351f96be0d61f755ce494f6969054e4f4d
Daniel Aschwanden <nimdanitro@gmail.com>
2015-10-03 21:08:12 +0200

    activityDashboards data

--- client/template/views/activityDashboard.js
+++ client/template/views/activityDashboard.js
@@ -7,37 +7,65 @@ if (Meteor.isClient) {
 }
 
 Template.activityDashboard.helpers({
-        days: function() {
-            var dates = ActivityData.find({type: 'session'}).map(function(session){
-                    return moment(session.start).format('LL');
-                }
-            );
-            dates = _.uniq(dates);
-            var test = _.map(dates, function(value, index){ return {date: value, id: index}; });
-            return test;
-        },
-        session: function(elem) {
-            return ActivityData.find({type: 'session'});
-        },
-        circularOptions : function() {
-            console.log("Graph circular Options");
-            console.log(this);
+    sessiondata: function() {
+        var dates = ActivityData.find({
+            type: 'session'
+        }).map(function(session) {
+            return moment(session.start).format('LL');
+        });
+        console.log("dates: ", dates);
+        dates = _.uniq(dates);
+        return _.map(dates, function(value, index) {
             return {
-                'canvasSize': 250,
-                'arcWidth': 10,
-                'sessionValueKey': 'progressValue',
-                'tweenDuration': 300,
-                //'outerDivClass': 'dailyGraphOuter'+this.id,
-                'innerDivClass': 'dailyGraph'+this.id,
-                'containerId': 'dailyGraph'+this.id,
-                'sessionValueKey': 'dailyGraphValue'+this.id,
-                'sessionTextKey': 'dailyGraphText'+this.id,
+                date: value,
+                id: index,
+                sessions: getSessions(value)
             }
-        },
-        fillGraph : function(data) {
-            console.log("Fill graph");
-            console.log(data.hash.date_id);
-            Session.set('dailyGraphValue'+data.hash.date_id, 8);
-            Session.set('dailyGraphText'+data.hash.date_id, 'Progress');
-        },
+        });
+    },
+    circularOptions: function() {
+        console.log("Graph circular Options");
+        console.log(this);
+        return {
+            'canvasSize': 250,
+            'arcWidth': 10,
+            'sessionValueKey': 'progressValue',
+            'tweenDuration': 300,
+            //'outerDivClass': 'dailyGraphOuter'+this.id,
+            'innerDivClass': 'dailyGraph' + this.id,
+            'containerId': 'dailyGraph' + this.id,
+            'sessionValueKey': 'dailyGraphValue' + this.id,
+            'sessionTextKey': 'dailyGraphText' + this.id,
+        }
+    },
+    fillGraph: function(data) {
+        console.log("Fill graph");
+        console.log(data.hash.date_id);
+        Session.set('dailyGraphValue' + data.hash.date_id, 8);
+        Session.set('dailyGraphText' + data.hash.date_id, 'Progress');
+    },
+    isValidSession: function(session) {
+        if (_.notNull(session.start) && _.notNull(session.end)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 });
+
+Template.activityDashboardSessionList.helpers({
+    formatDate: function(date) {
+        return moment(date).format('LLL');
+    },
+});
+
+
+getSessions = function(day) {
+    var sessions = ActivityData.find({type: 'session'}).map(function(session){
+            if (moment(session.start).format('LL') === day ){
+                console.log("session", session);
+                return session;
+            }
+        });
+    return sessions;
+}
