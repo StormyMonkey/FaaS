c3d096cf487971a48fb925e8c88d571255763084
Guido Hungerbuehler <guh@open.ch>
2015-10-04 00:08:00 +0200

    More data

--- server/fixtures/activityData.js
+++ server/fixtures/activityData.js
@@ -4,6 +4,56 @@ if ( (ActivityData.find({type: 'session'}).count() === 0) && ( Meteor.users.find
     ActivityData.insert({
         userId: userId._id,
         type: 'session',
+        activity: 'Cycling',
+        activity_value: 20,
+        fake: 1,
+        start: new Date('2015-10-03T07:56:00.123Z'),
+        end: new Date('2015-10-03T08:40:00.123Z'),
+    });
+    
+    ActivityData.insert({
+        userId: userId._id,
+        type: 'session',
+        activity: 'Cycling',
+        activity_value: 20,
+        fake: 1,
+        start: new Date('2015-10-03T07:56:00.123Z'),
+        end: new Date('2015-10-03T08:40:00.123Z'),
+    });
+    
+    ActivityData.insert({
+        userId: userId._id,
+        type: 'session',
+        activity: 'Swimming',
+        activity_value: 10,
+        fake: 1,
+        start: new Date('2015-10-03T09:46:00.123Z'),
+        end: new Date('2015-10-03T09:59:00.123Z'),
+    });
+    
+    ActivityData.insert({
+        userId: userId._id,
+        type: 'session',
+        activity: 'Running',
+        activity_value: 20,
+        fake: 1,
+        start: new Date('2015-10-03T14:56:00.123Z'),
+        end: new Date('2015-10-03T15:30:00.123Z'),
+    });
+    
+    ActivityData.insert({
+        userId: userId._id,
+        type: 'session',
+        activity: 'Cycling',
+        activity_value: 10,
+        fake: 0,
+        start: new Date('2015-10-03T17:56:00.123Z'),
+        end: new Date('2015-10-03T18:10:00.123Z'),
+    });
+    
+    ActivityData.insert({
+        userId: userId._id,
+        type: 'session',
         activity: 'Running',
         activity_value: 30,
         fake: 0,
