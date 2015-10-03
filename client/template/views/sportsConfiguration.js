417aa95a599265d90aae8c0b9c12daab43023568
Simon Staeheli <simon.staeheli@gmail.com>
2015-10-03 06:59:42 +0200

    adding slider

--- client/template/views/sportsConfiguration.js
+++ client/template/views/sportsConfiguration.js
@@ -1,10 +1,24 @@
 if (Meteor.isClient) {
     Template.sportsConfiguration.helpers({
         sports: [
-            { text: "cycling",
-              active: "active" },
-            { text: "running" },
-            { text: "swimming" }
+            {   nr: 0,
+                text: "cycling",
+                active: "active"
+            },
+            {   nr: 1,
+                text: "running"
+            },
+            {   nr: 2,
+                text: "swimming"
+            }
         ]
     });
+
+    var slider = new Slider('#ex1', {
+    	formatter: function(value) {
+    		return 'Current value: ' + value;
+    	}
+    });
+
+    $('.slider').slider()
 }
