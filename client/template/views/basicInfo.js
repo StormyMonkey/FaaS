c838a890ea61c52bbfef420a87e3da462fa5fcd1
Simon Staeheli <simon.staeheli@gmail.com>
2015-10-03 17:32:46 +0200

    save basic information to mongo db

--- client/template/views/basicInfo.js
+++ client/template/views/basicInfo.js
@@ -1,1 +1,36 @@
+Template.basicInfo.events({
 
+    "submit form": function (event) {
+        event.preventDefault();
+
+        console.log(event);
+
+        var birthdate = event.target.birthdate.value;
+        var weight = event.target.weight.value;
+        var gender = event.target.gender;
+
+        var basic_information = {
+            birthdate: birthdate,
+            weight: weight,
+            gender: "w",
+        }
+        console.log(basic_information);
+
+        Meteor.call("updateBasicInformation", basic_information, function(error, result){
+            if(error){
+                return Errors.throw(error.reason);
+            }
+            else {
+                // Clear form
+                event.target.birthdate = "";
+                event.target.weight = "";
+                event.target.gender = "";
+
+                // Prevent default form submit
+                return false;
+            }
+        });
+
+        FlowRouter.go('/activityconfiguration');
+    }
+});
