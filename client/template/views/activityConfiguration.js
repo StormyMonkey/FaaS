dda4b5a4d75459f80cc311367db3868c7f11d4bd
Simon Staeheli <simon.staeheli@gmail.com>
2015-10-03 23:13:59 +0200

    store activity configuration in mongodb

--- client/template/views/activityConfiguration.js
+++ client/template/views/activityConfiguration.js
@@ -19,6 +19,29 @@ if (Meteor.isClient) {
         Template.activitySlider.events({
             "submit form": function (event) {
                 event.preventDefault();
+
+                var activity = Activity.findOne({_id: FlowRouter.getParam('id')});
+                var amb = $('#ambition-slider').data('slider').getValue();
+                var reg = $('#regularity-slider').data('slider').getValue();
+                var rand = $('#randomness-slider').data('slider').getValue();
+
+                var activity_configuration = {
+                    id: activity,
+                    settings: {
+                        amb: amb,
+                        reg: reg,
+                        rand: rand,
+                    },
+                };
+
+                Meteor.call("updateActivityConfiguration", activity_configuration, function(error, result){
+                    if(error){
+                        return Errors.throw(error.reason);
+                    }
+                });
+
+                console.log(activity_configuration);
+
                 FlowRouter.go('/activityconfiguration');
             }
         });
