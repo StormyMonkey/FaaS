0fcb1a7214f2780e77de30178a1913466174ff36
Daniel Aschwanden <nimdanitro@gmail.com>
2015-10-03 09:58:17 +0200

    prepare collections

--- client/template/views/sportsConfiguration.js
+++ client/template/views/sportsConfiguration.js
@@ -1,36 +1,18 @@
 if (Meteor.isClient) {
     Template.sportsConfiguration.helpers({
-        sports: [
-            {   nr: 0,
-                text: "cycling",
-                image: "cycle.gif",
-                active: "active"
-            },
-            {   nr: 1,
-                text: "running",
-                image: "run.gif",
-            },
-            {   nr: 2,
-                text: "swimming",
-                image: "swim.gif",
-            },
-            {   nr: 3,
-                text: "skating",
-                image: "skate.gif",
-            },
-            {   nr: 4,
-                text: "hiking",
-                image: "walk.gif",
-            }
-        ]
+        activities: function() {
+            return Activity.find({}).map( function(activity, index) {
+                activity.nr = index;
+                return activity;
+            });
+        },
     });
 
     Meteor.startup(function() {
         var slider = new Slider('#ex1', {
-        	formatter: function(value) {
-        		return 'Current value: ' + value;
-        	}
+            formatter: function(value) {
+                return 'Current value: ' + value;
+            }
         });
     });
-
 }
